d74fa06fd05b1759248d8f7759e58c43cbc98c1c
WinGo <WinGo@DESKTOP-QR87BG2>
2018-05-21 13:29:36 +0800

    添加项目文件。

---
+++ Lambda/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        public delegate bool ComparisonHandler(int First, int Second);
+
+        static void Main(string[] args)
+        {
+            int[] items = new int[8];
+            for (int i = 0; i < items.Length; i++)
+            {
+                System.Console.Write("Enter an interger：");
+                items[i] = int.Parse(System.Console.ReadLine());
+            }
+            Bubblesort(items, GreaterThan);
+            for(int i = 0;i<items.Length;i++)
+            {
+                System.Console.WriteLine(items[i]);
+            }
+        }
+
+        public static void Bubblesort(int[] items,ComparisonHandler comparisonMethod)
+        {
+            int temp;
+
+            if(comparisonMethod ==null)
+            {
+                throw new ArgumentNullException("compaisonMethod");
+            }
+
+            if (items == null)
+            {
+                return;
+            }
+
+            for (int i = items.Length - 1; i >= 0; i--)
+            {
+                for(int j = 1;j<=i;j++)
+                {
+                    if(comparisonMethod(items[j-1],items[j]))
+                    {
+                        temp = items[j - 1];
+                        items[j - 1] = items[j];
+                        items[j] = temp;
+                    }
+                }
+            }
+           
+        }
+
+        public static bool GreaterThan(int First, int Second)
+        {
+            return First > Second;
+        }
+    }
+}
